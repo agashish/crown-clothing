73b992ce1c3c293d41f689d7573105ac0e2aba73
Ashish Kumar <ag.ashishaggarwal@gmail.com>
2020-02-15 18:51:57 +0530

    Add Delivery | Menu-Item & Styling Done with zoom effect on hover

---
+++ src/pages/homepage.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import DirectiveMenu from './../components/directory-menu/directory-menu.component';
+import './homepage.styles.scss';
+
+const HomePage = () => (
+    <div className="homepage">
+        <DirectiveMenu />
+    </div>
+)
+export default HomePage;
