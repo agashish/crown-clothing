322d1ff8ed5a4e306844473fc38e8d92866f01ff
Ashish Kumar <ag.ashishaggarwal@gmail.com>
2020-02-22 09:54:40 +0530

    Sign-in component, form-input component adding new scss with mixins & @include methods to make reusabel css code in files. also add custom button component

--- src/App.js
+++ src/App.js
@@ -5,19 +5,21 @@ import './App.css';
 
 import ShopPage from './pages/shop-page/shop-page.component';
 import Header from './components/header/header.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 const HatsPage = props => {
   return (
     <h1>Hats Page - {props.match.params.menuItem}</h1>
   )
 }
-function App() {
+function App() { 
   return (
     <div>
       <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />  
+        <Route path="/signin" component={SignInAndSignUp} />
       </Switch>
     </div>
   );
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -19,6 +19,10 @@ const Header = () => {
                 <Link className='option' to="/shop">
                     CONTACT
                 </Link>
+
+                <Link className='option' to="/signin">
+                    SIGNIN
+                </Link>
             </div>   
         </div>
     )
