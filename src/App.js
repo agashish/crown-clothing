0255f6533bf4ca201bea1e53dcc8b007ca92583e
Ashish Kumar <ag.ashishaggarwal@gmail.com>
2020-02-29 23:21:38 +0530

    Redirect and render are both used and also make the types dynamic

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import HomePage from './pages/homepage.component';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import {connect} from 'react-redux';
 import { setCurrentUser } from './redux/user-reducer/user.actions';
 import './App.css';
@@ -63,14 +63,21 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />  
-          <Route path="/signin" component={SignInAndSignUp} />
+          <Route path="/signin" render={() => this.props.currentUser ? (<Redirect to="/" />) : (<SignInAndSignUp />)} />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({user}) => ({
+  currentUser: user.currentUser   
+}) 
+
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: (user) => dispatch(setCurrentUser(user))
 })
-export default connect(null, mapDispatchToProps)(App);
+export default connect(
+  mapStateToProps, 
+  mapDispatchToProps)(App);
